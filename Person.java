8a249687dbe7d29d512ac085c2dd05dfeff40f8a
Aerionnas <34111182+Aerionnas@users.noreply.github.com>
2025-03-23 17:20:29 -0400

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,24 @@
+package Programming1;
+
+/*This program takes in 5 different variables which store information about the person
+ * and prints it to the console.
+ */
+public class Person {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String firstName = "Aerionna"; //this is the person's first name
+		String lastName = "Stephenson";//this is the person's last name
+		String addy = "238 George Parks Hwy";//this is the person's address
+		String city = "Healy";//this is the person's city name
+		int zipcode = 99743; //this is the person's zipcode
+		
+		//each variable is printed on a new line
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(addy);
+		System.out.println(city);
+		System.out.println(zipcode);
+	}
+
+}
